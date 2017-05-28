fc29c0a242ff15a70eb84bb8518ff51d78721307
jankrib <jan@crondale.no>
2017-05-28 20:33:22 +0200

    Start to get the angular content in

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,46 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var merge = require('merge2');
 
+var spawn = require('child_process').spawn;
+var node;
+
+/**
+ * $ gulp server
+ * description: launch the server. If there's a server already running, kill it.
+ */
+gulp.task('server', function() {
+  if (node)
+    node.kill()
+
+  node = spawn('node', ['bin/startup.js'], {stdio: 'inherit'})
+
+  node.on('close', function (code) {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
+})
+
 var tsProject = ts.createProject({
     declaration: true
 });
 
-gulp.task('scripts', function() {
+gulp.task('build', function() {
     var tsResult = gulp.src('src/*.ts')
         .pipe(tsProject());
 
     return merge([ // Merge the two output streams, so this task is finished when the IO of both operations is done.
-        tsResult.dts.pipe(gulp.dest('build')),
-        tsResult.js.pipe(gulp.dest('build'))
+        tsResult.dts.pipe(gulp.dest('dist')),
+        tsResult.js.pipe(gulp.dest('dist'))
     ]);
 });
 
-gulp.task('watch', ['scripts'], function() {
-    gulp.watch('src/*.ts', ['scripts']);
+gulp.task('watch', ['build', 'server'], function() {
+    gulp.watch('src/*.ts', ['build', 'server']);
 });
+
+// clean up if an error goes unhandled.
+process.on('exit', function() {
+    if (node)
+      node.kill()
+})
---
+++ src/server.ts
@@ -0,0 +1,102 @@
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as express from "express";
+import * as path from "path";
+import errorHandler = require("errorhandler");
+import methodOverride = require("method-override");
+
+declare var __dirname;
+
+/**
+ * The server.
+ *
+ * @class Server
+ */
+export class Server {
+
+  public app: express.Application;
+
+  /**
+   * Bootstrap the application.
+   *
+   * @class Server
+   * @method bootstrap
+   * @static
+   * @return {ng.auto.IInjectorService} Returns the newly created injector for this app.
+   */
+  public static bootstrap(): Server {
+    return new Server();
+  }
+
+  /**
+   * Constructor.
+   *
+   * @class Server
+   * @constructor
+   */
+  constructor() {
+    //create expressjs application
+    this.app = express();
+
+    //configure application
+    this.config();
+
+    //add api
+    this.api();
+
+    this.app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../system', 'index.html'));
+    });
+  }
+
+  /**
+   * Create REST API routes
+   *
+   * @class Server
+   * @method api
+   */
+  public api() {
+    var router = express.Router();
+
+    router.get('/', function(req, res) {
+        res.json({ message: 'Welcome to our api!' });
+    });
+
+    this.app.use('/api', router);
+  }
+
+  /**
+   * Configure application
+   *
+   * @class Server
+   * @method config
+   */
+  public config() {
+    var app = this.app;
+
+    app.use(express.static(path.join(__dirname, "public")));
+
+    app.use('/node_modules', express.static('node_modules'))
+    app.use('/system', express.static('system'))
+
+    // configure app to use bodyParser()
+    // this will let us get the data from a POST
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    //use cookie parker middleware middlware
+    app.use(cookieParser("SECRET_GOES_HERE"));
+
+    //use override middlware
+    app.use(methodOverride());
+
+    //catch 404 and forward to error handler
+    app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+        err.status = 404;
+        next(err);
+    });
+
+    //error handling
+    app.use(errorHandler());
+  }
+}
