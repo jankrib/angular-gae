2586194cf6e3d5862665fedd571febd76b26327e
jankrib <jan@crondale.no>
2017-05-28 19:28:02 +0200

    Make the gulp build task for ts

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var merge = require('merge2');
+
+var tsProject = ts.createProject({
+    declaration: true
+});
+
+gulp.task('scripts', function() {
+    var tsResult = gulp.src('src/*.ts')
+        .pipe(tsProject());
+
+    return merge([ // Merge the two output streams, so this task is finished when the IO of both operations is done.
+        tsResult.dts.pipe(gulp.dest('build')),
+        tsResult.js.pipe(gulp.dest('build'))
+    ]);
+});
+
+gulp.task('watch', ['scripts'], function() {
+    gulp.watch('src/*.ts', ['scripts']);
+});
